c30d0452f1fd15f1957e8bcd4d785b9530ebdbe4
jamesta696 <jaysmith696@gmail.com>
2018-12-10 11:09:42 -0500

    react benchmark template

---
+++ src/users.js
@@ -0,0 +1,18 @@
+var USERS = {
+    contacts: [
+        {
+            name: "john smith",
+            age: "25",
+            location: "new york",
+            id: 1
+        },
+        {
+            name: "sally sanders",
+            age: "35",
+            location: "florida",
+            id: 2
+        }
+    ]
+};
+
+export { USERS };
